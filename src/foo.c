5f8d1c1ce44507800e5f696d98584c1223fbcf21
Raphael Shin <rshin@z4r6p7.42lyon.fr>
2025-07-24 16:09:03 +0200

    gettime, usleep works

--- src/foo.c
+++ src/foo.c
@@ -6,7 +6,7 @@
 /*   By: rshin <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 14:42:21 by rshin             #+#    #+#             */
-/*   Updated: 2025/07/23 17:40:29 by rshin            ###   ########.fr       */
+/*   Updated: 2025/07/24 16:08:40 by rshin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,62 +20,146 @@
 # include <sys/time.h>
 # include <time.h>
 
+typedef pthread_mutex_t t_mtx;
+
+typedef struct	s_fork
+{
+	bool	is_taken;
+	t_mtx	mutex;
+}	t_fork;
+
 typedef struct	s_philo
 {
 	pthread_t		tid;
 	int				id;
-	pthread_mutex_t	*l_fork;
-	pthread_mutex_t	*r_fork;
-	bool			start;
+	struct s_fork	*l_fork;
+	struct s_fork	*r_fork;
+	bool			is_dead;
+	struct s_param	*param;
 }	t_phi;
+
+typedef struct	s_param
+{
+	struct s_philo	*philo;
+	pthread_t		monitor;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	bool			start;
+	bool			end;
+}	t_param;
 
 void	*routine(void *arg)
 {
+	int				ms;
+	int				last_time;
+	struct timeval	tv;
 	t_phi	*p;
+//	int		count;
 
+//	count = 0;
 	p =	(t_phi *)arg;
 	while (true)
 	{
-		if (p->start == true)
+//		count++;
+		if (p->param->start == true)
 			break;
 		else
 			continue;
 	}
-	printf("here is philo %d\n", p->id);
-	pthread_mutex_lock(p->l_fork);
-	pthread_mutex_lock(p->r_fork);
-	printf("eat\n");
-	pthread_mutex_unlock(p->l_fork);
-	pthread_mutex_unlock(p->r_fork);
+//	printf("%d\n", count);
+	while (true)
+	{
+		gettimeofday(&tv, NULL);
+		last_time = tv.tv_usec;
+		if (p->id % 2 == 0)
+		{
+			pthread_mutex_lock(&p->l_fork->mutex);
+			pthread_mutex_lock(&p->r_fork->mutex);
+		}
+		else
+		{
+			pthread_mutex_lock(&p->r_fork->mutex);
+			pthread_mutex_lock(&p->l_fork->mutex);
+		}
+		gettimeofday(&tv, NULL);
+		ms = tv.tv_usec - last_time;
+		printf("%d philo %d is thinking\n", ms, p->id);
+		usleep(param->time_to_eat);
+		gettimeofday(&tv, NULL);
+		last_time = tv.tv_usec;
+		ms = tv.tv_usec - last_time;
+		printf("%d philo %d is eating\n", ms, p->id);
+		pthread_mutex_unlock(&p->l_fork->mutex);
+		pthread_mutex_unlock(&p->r_fork->mutex);
+		gettimeofday(&tv, NULL);
+		last_time = tv.tv_usec;
+		ms = tv.tv_usec - last_time;
+		printf("%d philo %d is sleeping\n", ms, p->id);
+		usleep(param->time_to_sleep);
+		if (p->param->end == true)
+			break;
+	}
 	return (NULL);
+}
+
+void	*monitor(void *arg)
+{
+	t_param	*param;
+	int		i;
+
+	param = (t_param *)arg;
+	while (true)
+	{
+		i = 0;
+		while (i < 3)
+		{
+			if (param->philo[i].is_dead)
+			{
+				printf("philo %d is dead, end of simulation", i);
+				param->end = true;
+			}
+			i++;
+		}
+	}
 }
 
 int	main(int argc, char **argv)
 {
+	t_param	param;
 	t_phi	p[3];
-	pthread_mutex_t	fork[3];
+	t_fork	fork[3];
 	int		i;
 
 	(void)argc;
 	(void)argv;
+	memset(&param, 0, sizeof(t_param));
+	param.philo = p;
+	param.time_to_die = 200;
+	param.time_to_eat = 200;
+	param.time_to_sleep = 200;
 	i = 0;
 	while (i < 3)
 	{
 		memset(&p[i], 0, sizeof(t_phi));
+		memset(&fork[i], 0, sizeof(t_fork));
 		p[i].id = i + 1;
-		pthread_mutex_init((&fork[i]), NULL);
+		pthread_mutex_init((&fork[i].mutex), NULL);
+		p[i].param = &param;
 		i++;
 	}
 	i = 0;
 	while (i < 3)
 	{
-		p[i].l_fork = &fork[i - 1];
+		p[i].l_fork = &fork[i];
 		if (i == 3 - 1)
 			p[i].r_fork = &fork[0];
 		else
-			p[i].r_fork = &fork[i / 3 + i % 3];
+			p[i].r_fork = &fork[(i + 1) / 3 + (i + 1) % 3];
 		i++;
 	}
+	if (pthread_create(&param.monitor, NULL, &monitor, &param))
+		return (1);
 	i = 0;
 	while (i < 3)
 	{
@@ -83,13 +167,13 @@ int	main(int argc, char **argv)
 			return (1);
 		i++;
 	}
-	(*p).start = true;
+	param.start = true;
 	i = 0;
 	while (i < 3)
 	{
 		if (pthread_join(p[i].tid, NULL))
 			return (1);
-		pthread_mutex_destroy(&fork[i]);
+		pthread_mutex_destroy(&fork[i].mutex);
 		i++;
 	}
 	return (0);
