7c45a02e90802b66a47298e424081ce046966e98
Raphael Shin <rshin@z1r3p4.42lyon.fr>
2025-08-19 20:35:49 +0200

    changing routine wip

--- src/run.c
+++ src/run.c
@@ -6,7 +6,7 @@
 /*   By: rshin <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 09:39:46 by rshin             #+#    #+#             */
-/*   Updated: 2025/08/19 19:10:21 by rshin            ###   ########.fr       */
+/*   Updated: 2025/08/19 20:26:26 by rshin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,17 +31,6 @@ void	print_output(long time, int id, char *msg)
 	pthread_mutex_unlock(&p->rts->print_mtx);
 }
 
-static bool	action(t_phi *p, char *msg)
-{
-	long	time;
-
-	if (!check_death(p->rts))
-		return (false);
-	time = get_time() - p->cfg->start;
-	print_output(time, p->id, msg);
-	return (true);
-}
-
 bool	take_fork(t_fork *fork)
 {
 	pthread_mutex_lock(&fork->mtx);
@@ -55,59 +44,69 @@ bool	take_fork(t_fork *fork)
 	return (false);
 }
 
-bool	drop_fork(t_fork *fork)
+void	drop_fork(t_fork *fork)
 {
-	
+	pthread_mutex_lock(&fork->mtx);
+	fork->is_taken = false;
+	pthread_mutex_unlock(&fork->mtx);
 }
 
-bool	take_forks(t_phi *p)
+bool	assign_forks(t_phi *p)
 {
 	int	first;
 	int	second;
 
 	while (true)
 	{
-		if (!check_death(p->rts))
-			return (false);
 		first = p->id % 2;
 		second = 1 - first;
 		pthread_mutex_lock(p->forks[first]->mtx);
-		if (p->forks[first]->is_taken == false)
+		if (!take_fork(p->forks[first]))
+			continue;
+		print_output(time, p->id, "has taken a fork");
+		if (!take_fork(p->forks[second]))
 		{
-			p->forks[first]->is_taken = true;
-			pthread_mutex_unlock(p->forks[first]->mtx);
-			if (!action(p, "has taken a fork"))
-				return (false);
-			pthread_mutex_lock(p->forks[second]->mtx);
-			if (p->forks[second]->is_taken == false)
-			{
-				p->forks[second]->is_taken = true;
-				pthread_mutex_unlock(p->forks[second]->mtx);
-				if (!action(p, "has taken a fork"))
-					return (false);
-				break;
-			}
-			pthread_mutex_unlock(p->forks[second]->mtx);
-			p->forks[first]->is_taken = false;
+			drop_fork(p->forks[first]);
+			continue;
 		}
-		pthread_mutex_unlock(p->forks[first]->mtx);
+		print_output(time, p->id, "has taken a fork");
+		drop_fork(p->forks[first]);
+		drop_fork(p->forks[second]);
 	}
 	return (true);
 }
 
-bool	drop_forks(t_phi *p)
+bool	eat(t_phi *p)
 {
-	int	first;
-	int	second;
-	
-	first = p->id % 2;
-	second = 1 - first;
-	pthread_mutex_lock(p->forks[first]->mtx);
-	p->forks[first]->is_taken = false;
-	pthread_mutex_unlock(p->forks[first]->mtx);
-	pthread_mutex_lock(p->forks[second]->mtx);
-	p->forks[second]->is_taken = false;
-	pthread_mutex_unlock(p->forks[second]->mtx);
+	int	time;
+
+	if (!check_death(p->rts))
+		return (false);
+	time = get_time() - p->cfg->start;
+	print_output(time, p->id, "is eating");
+	smart_sleep(p->cfg->time_to_eat);
+	pthread_mutex_lock(&p->meal.mtx);
+	p->meal.last = time;
+	p->meal.count++;
+	if (p->meal.count == p->cfg->max_meals)
+	{
+		pthread_mutex_unlock(&p->meal.mtx);
+		break;
+	}
+	pthread_mutex_unlock(&p->meal.mtx);
+	drop_forks(p);
+	return (true);
+}
+
+bool	sleep(t_phi *p)
+{
+	int	time;
+
+	if (!check_death(p->rts))
+		return (false);
+	time = get_time() - p->cfg->start;
+	print_output(time, p->id, "is sleeping");
+	smart_sleep(p->cfg->time_to_sleep);
 }
 
 void	*philo_loop(void *arg)
@@ -120,29 +119,12 @@ void	*philo_loop(void *arg)
 //		usleep(1000);
 	while (true)
 	{
-		if (!action(p, "is thinking", 0))
+		if (!think(p))
 			break;
-		if (!take_forks(p))
+		if (!eat(p))
 			break;
-		if (!action(p, "is eating", p->cfg->time_to_eat))
-		{
-			drop_forks(p);
+		if (!sleep(p)
 			break ;
-		}
-		smart_sleep(p->cfg->time_to_eat);
-		pthread_mutex_lock(&p->meal.mtx);
-		p->meal.last = time;
-		p->meal.count++;
-		if (p->meal.count == p->cfg->max_meals)
-		{
-			pthread_mutex_unlock(&p->meal.mtx);
-			break;
-		}
-		pthread_mutex_unlock(&p->meal.mtx);
-		drop_forks(p);
-		if (!action(p, "is sleeping")
-			break ;
-		smart_sleep(p->cfg->time_to_sleep);
 	}
 	return (NULL);
 }
